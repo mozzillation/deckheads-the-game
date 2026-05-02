7b4fd33a8b942e3265af6c6363f6f1c5ce943d3e
Giuliano Mozzillo <giulianomozzillo@live.it>
2026-05-02 17:06:00 +0200

    refactor: delegate monster hit logic to MonsterStrategy

--- scripts/combat/combat_manager.gd
+++ scripts/combat/combat_manager.gd
@@ -6,19 +6,29 @@ extends RefCounted
 # ---
 
 enum Target { PLAYER, MONSTER }
+enum Stage { START, DEAL, PLAYER_TURN, MONSTER_TURN, DONE }
 
 # ---
 # Signals
 # ---
 
 signal round_started
+signal stage_changed(new_stage: Stage)
+signal card_drawn(card: Card, target: Target)
+signal monster_hand_revealed
+signal player_bust
+signal monster_bust
+signal round_resolved(winner: Target)
 signal player_damaged(amount: int, new_hp: int)
 signal player_healed(amount: int, new_hp: int)
 signal player_died
+signal monster_died
 
 # ---
 # Variables
 # ---
+
+var stage: Stage = Stage.START
 
 var monster: Monster
 var player: Player
@@ -47,7 +57,11 @@ func start_round() -> void:
 	player_hand = Hand.new()
 	monster_hand = Hand.new()
 	_deal_index = 0
+	_set_stage_to(Stage.DEAL)
 	round_started.emit()
+
+func finish_deal() -> void:
+	_set_stage_to(Stage.PLAYER_TURN)
 
 func has_cards_to_deal() -> bool:
 	return _deal_index < _DEAL_ORDER.size()
@@ -63,6 +77,34 @@ func deal_next() -> Dictionary:
 	_deal_index += 1
 	return { card = card, target = target, face_down = face_down }
 
+func player_hit() -> void:
+	var card := deck.draw()
+	player_hand.add(card, false)
+	card_drawn.emit(card, Target.PLAYER)
+	if player_hand.is_burst():
+		player_bust.emit()
+		_resolve_round(Target.MONSTER)
+
+func player_stand() -> void:
+	monster_hand.reveal_all()
+	monster_hand_revealed.emit()
+	_set_stage_to(Stage.MONSTER_TURN)
+
+func monster_hit() -> void:
+	var card := deck.draw()
+	monster_hand.add(card, false)
+	card_drawn.emit(card, Target.MONSTER)
+	if monster_hand.is_burst():
+		monster_bust.emit()
+		_resolve_round(Target.PLAYER)
+
+func monster_stand() -> void:
+	var wins := CombatResolution.player_wins_standoff(player_hand.score(), monster_hand.score())
+	_resolve_round(Target.PLAYER if wins else Target.MONSTER)
+
+func monster_should_hit() -> bool:
+	return MonsterStrategy.should_hit(monster.play_style, monster_hand.score())
+
 func damage_player(amount: int) -> void:
 	player.take_damage(amount)
 	if not player.is_alive():
@@ -73,3 +115,22 @@ func damage_player(amount: int) -> void:
 func heal_player(amount: int) -> void:
 	player.heal(amount)
 	player_healed.emit(amount, player.hp)
+
+# ---
+# Functions (Private)
+# ---
+
+func _resolve_round(winner: Target) -> void:
+	_set_stage_to(Stage.DONE)
+	round_resolved.emit(winner)
+	if winner == Target.MONSTER:
+		damage_player(CombatResolution.damage_to_player(monster))
+	else:
+		monster.take_damage(CombatResolution.damage_to_monster(player))
+		if not monster.is_alive():
+			monster_died.emit()
+
+func _set_stage_to(_stage: Stage) -> void:
+	if stage == _stage: return
+	stage = _stage
+	stage_changed.emit(stage)
---
+++ scripts/combat/combat_resolution.gd
@@ -0,0 +1,14 @@
+class_name CombatResolution
+
+# ---
+# Functions
+# ---
+
+static func player_wins_standoff(player_score: int, monster_score: int) -> bool:
+	return player_score > monster_score
+
+static func damage_to_player(monster: Monster) -> int:
+	return monster.base_damage
+
+static func damage_to_monster(_player: Player) -> int:
+	return 1
